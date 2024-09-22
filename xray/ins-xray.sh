5c3d22538579529c20cf759486775a14958a1c4d
titis69 <117496052+titis69@users.noreply.github.com>
2024-09-22 16:29:00 +0700

    Update ins-xray.sh

--- xray/ins-xray.sh
+++ xray/ins-xray.sh
@@ -27,12 +27,12 @@ date
 echo ""
 domain=$(cat /root/domain)
 sleep 1
-mkdir -p /etc/xray 
+mkdir -p /etc/xray
 echo -e "[ ${green}INFO${NC} ] Checking... "
 apt install iptables iptables-persistent -y
 sleep 1
 echo -e "[ ${green}INFO$NC ] Setting ntpdate"
-ntpdate pool.ntp.org 
+ntpdate pool.ntp.org
 timedatectl set-ntp true
 sleep 1
 echo -e "[ ${green}INFO$NC ] Enable chronyd"
@@ -49,7 +49,7 @@ chronyc sourcestats -v
 chronyc tracking -v
 echo -e "[ ${green}INFO$NC ] Setting dll"
 apt clean all && apt update
-apt install curl socat xz-utils wget apt-transport-https gnupg gnupg2 gnupg1 dnsutils lsb-release -y 
+apt install curl socat xz-utils wget apt-transport-https gnupg gnupg2 gnupg1 dnsutils lsb-release -y
 apt install socat cron bash-completion ntpdate -y
 ntpdate pool.ntp.org
 apt -y install chrony
@@ -107,247 +107,247 @@ uuid=$(cat /proc/sys/kernel/random/uuid)
 cat > /etc/xray/config.json << END
 {
   "log" : {
-    "access": "/var/log/xray/access.log",
-    "error": "/var/log/xray/error.log",
-    "loglevel": "warning"
+	"access": "/var/log/xray/access.log",
+	"error": "/var/log/xray/error.log",
+	"loglevel": "warning"
   },
   "inbounds": [
-      {
-      "listen": "127.0.0.1",
-      "port": 10085,
-      "protocol": "dokodemo-door",
-      "settings": {
-        "address": "127.0.0.1"
-      },
-      "tag": "api"
-    },
+  	{
+  	"listen": "127.0.0.1",
+  	"port": 10085,
+  	"protocol": "dokodemo-door",
+  	"settings": {
+    	"address": "127.0.0.1"
+  	},
+  	"tag": "api"
+	},
    {
-     "listen": "/run/xray/vless_ws.sock",
-     "protocol": "vless",
-      "settings": {
-          "decryption":"none",
-            "clients": [
-               {
-                 "id": "${uuid}"                 
+ 	"listen": "/run/xray/vless_ws.sock",
+ 	"protocol": "vless",
+  	"settings": {
+      	"decryption":"none",
+        	"clients": [
+           	{
+             	"id": "${uuid}"            	 
 #vless
-             }
-          ]
-       },
-       "streamSettings":{
-         "network": "ws",
-            "wsSettings": {
-                "path": "/vlessws"
-          }
-        }
-     },
-     {
-     "listen": "/run/xray/vmess_ws.sock",
-     "protocol": "vmess",
-      "settings": {
-            "clients": [
-               {
-                 "id": "${uuid}",
-                 "alterId": 0
+         	}
+      	]
+   	},
+   	"streamSettings":{
+     	"network": "ws",
+        	"wsSettings": {
+            	"path": "/vlessws"
+      	}
+    	}
+ 	},
+ 	{
+ 	"listen": "/run/xray/vmess_ws.sock",
+ 	"protocol": "vmess",
+  	"settings": {
+        	"clients": [
+           	{
+             	"id": "${uuid}",
+             	"alterId": 0
 #vmess
-             }
-          ]
-       },
-       "streamSettings":{
-         "network": "ws",
-            "wsSettings": {
-                "path": "/vmess"
-          }
-        }
-     },
-    {
-      "listen": "/run/xray/trojan_ws.sock",
-      "protocol": "trojan",
-      "settings": {
-          "decryption":"none",		
-           "clients": [
-              {
-                 "password": "${uuid}"
+         	}
+      	]
+   	},
+   	"streamSettings":{
+     	"network": "ws",
+        	"wsSettings": {
+            	"path": "/vmess"
+      	}
+    	}
+ 	},
+	{
+  	"listen": "/run/xray/trojan_ws.sock",
+  	"protocol": "trojan",
+  	"settings": {
+      	"decryption":"none",   	 
+       	"clients": [
+          	{
+             	"password": "${uuid}"
 #trojanws
-              }
-          ],
-         "udp": true
-       },
-       "streamSettings":{
-           "network": "ws",
-           "wsSettings": {
-               "path": "/trojan-ws"
-            }
-         }
-     },
-    {
-         "listen": "127.0.0.1",
-        "port": "30300",
-        "protocol": "shadowsocks",
-        "settings": {
-           "clients": [
-           {
-           "method": "aes-128-gcm",
-          "password": "${uuid}"
+          	}
+      	],
+     	"udp": true
+   	},
+   	"streamSettings":{
+       	"network": "ws",
+       	"wsSettings": {
+           	"path": "/trojan-ws"
+        	}
+     	}
+ 	},
+	{
+     	"listen": "127.0.0.1",
+    	"port": "30300",
+    	"protocol": "shadowsocks",
+    	"settings": {
+       	"clients": [
+       	{
+       	"method": "aes-128-gcm",
+      	"password": "${uuid}"
 #ssws
-           }
-          ],
-          "network": "tcp,udp"
-       },
-       "streamSettings":{
-          "network": "ws",
-             "wsSettings": {
-               "path": "/ss-ws"
-           }
-        }
-     },	
-      {
-        "listen": "/run/xray/vless_grpc.sock",
-        "protocol": "vless",
-        "settings": {
-         "decryption":"none",
-           "clients": [
-             {
-               "id": "${uuid}"
+       	}
+      	],
+      	"network": "tcp,udp"
+   	},
+   	"streamSettings":{
+      	"network": "ws",
+         	"wsSettings": {
+           	"path": "/ss-ws"
+       	}
+    	}
+ 	},    
+  	{
+    	"listen": "/run/xray/vless_grpc.sock",
+    	"protocol": "vless",
+    	"settings": {
+     	"decryption":"none",
+       	"clients": [
+         	{
+           	"id": "${uuid}"
 #vlessgrpc
-             }
-          ]
-       },
-          "streamSettings":{
-             "network": "grpc",
-             "grpcSettings": {
-                "serviceName": "vless-grpc"
-           }
-        }
-     },
-     {
-      "listen": "/run/xray/vmess_grpc.sock",
-     "protocol": "vmess",
-      "settings": {
-            "clients": [
-               {
-                 "id": "${uuid}",
-                 "alterId": 0
+         	}
+      	]
+   	},
+      	"streamSettings":{
+         	"network": "grpc",
+         	"grpcSettings": {
+            	"serviceName": "vless-grpc"
+       	}
+    	}
+ 	},
+ 	{
+  	"listen": "/run/xray/vmess_grpc.sock",
+ 	"protocol": "vmess",
+  	"settings": {
+        	"clients": [
+           	{
+             	"id": "${uuid}",
+             	"alterId": 0
 #vmessgrpc
-             }
-          ]
-       },
-       "streamSettings":{
-         "network": "grpc",
-            "grpcSettings": {
-                "serviceName": "vmess-grpc"
-          }
-        }
-     },
-     {
-        "listen": "/run/xray/trojan_grpc.sock",
-        "protocol": "trojan",
-        "settings": {
-          "decryption":"none",
-             "clients": [
-               {
-                 "password": "${uuid}"
+         	}
+      	]
+   	},
+   	"streamSettings":{
+     	"network": "grpc",
+        	"grpcSettings": {
+            	"serviceName": "vmess-grpc"
+      	}
+    	}
+ 	},
+ 	{
+    	"listen": "/run/xray/trojan_grpc.sock",
+    	"protocol": "trojan",
+    	"settings": {
+      	"decryption":"none",
+         	"clients": [
+           	{
+             	"password": "${uuid}"
 #trojangrpc
-               }
-           ]
-        },
-         "streamSettings":{
-         "network": "grpc",
-           "grpcSettings": {
-               "serviceName": "trojan-grpc"
-         }
-      }
+           	}
+       	]
+    	},
+     	"streamSettings":{
+     	"network": "grpc",
+       	"grpcSettings": {
+           	"serviceName": "trojan-grpc"
+     	}
+  	}
    },
    {
-    "listen": "127.0.0.1",
-    "port": "30310",
-    "protocol": "shadowsocks",
-    "settings": {
-        "clients": [
-          {
-             "method": "aes-128-gcm",
-             "password": "${uuid}"
+	"listen": "127.0.0.1",
+	"port": "30310",
+	"protocol": "shadowsocks",
+	"settings": {
+    	"clients": [
+      	{
+         	"method": "aes-128-gcm",
+         	"password": "${uuid}"
 #ssgrpc
-           }
-         ],
-           "network": "tcp,udp"
-      },
-    "streamSettings":{
-     "network": "grpc",
-        "grpcSettings": {
-           "serviceName": "ss-grpc"
-          }
-       }
-    }	
+       	}
+     	],
+       	"network": "tcp,udp"
+  	},
+	"streamSettings":{
+ 	"network": "grpc",
+    	"grpcSettings": {
+       	"serviceName": "ss-grpc"
+      	}
+   	}
+	}    
   ],
   "outbounds": [
-    {
-      "protocol": "freedom",
-      "settings": {}
-    },
-    {
-      "protocol": "blackhole",
-      "settings": {},
-      "tag": "blocked"
-    }
+	{
+  	"protocol": "freedom",
+  	"settings": {}
+	},
+	{
+  	"protocol": "blackhole",
+  	"settings": {},
+  	"tag": "blocked"
+	}
   ],
   "routing": {
-    "rules": [
-      {
-        "type": "field",
-        "ip": [
-          "0.0.0.0/8",
-          "10.0.0.0/8",
-          "100.64.0.0/10",
-          "169.254.0.0/16",
-          "172.16.0.0/12",
-          "192.0.0.0/24",
-          "192.0.2.0/24",
-          "192.168.0.0/16",
-          "198.18.0.0/15",
-          "198.51.100.0/24",
-          "203.0.113.0/24",
-          "::1/128",
-          "fc00::/7",
-          "fe80::/10"
-        ],
-        "outboundTag": "blocked"
-      },
-      {
-        "inboundTag": [
-          "api"
-        ],
-        "outboundTag": "api",
-        "type": "field"
-      },
-      {
-        "type": "field",
-        "outboundTag": "blocked",
-        "protocol": [
-          "bittorrent"
-        ]
-      }
-    ]
+	"rules": [
+  	{
+    	"type": "field",
+    	"ip": [
+      	"0.0.0.0/8",
+      	"10.0.0.0/8",
+      	"100.64.0.0/10",
+      	"169.254.0.0/16",
+      	"172.16.0.0/12",
+      	"192.0.0.0/24",
+      	"192.0.2.0/24",
+      	"192.168.0.0/16",
+      	"198.18.0.0/15",
+      	"198.51.100.0/24",
+      	"203.0.113.0/24",
+      	"::1/128",
+      	"fc00::/7",
+      	"fe80::/10"
+    	],
+    	"outboundTag": "blocked"
+  	},
+  	{
+    	"inboundTag": [
+      	"api"
+    	],
+    	"outboundTag": "api",
+    	"type": "field"
+  	},
+  	{
+    	"type": "field",
+    	"outboundTag": "blocked",
+    	"protocol": [
+      	"bittorrent"
+    	]
+  	}
+	]
   },
   "stats": {},
   "api": {
-    "services": [
-      "StatsService"
-    ],
-    "tag": "api"
+	"services": [
+  	"StatsService"
+	],
+	"tag": "api"
   },
   "policy": {
-    "levels": {
-      "0": {
-        "statsUserDownlink": true,
-        "statsUserUplink": true
-      }
-    },
-    "system": {
-      "statsInboundUplink": true,
-      "statsInboundDownlink": true,
-      "statsOutboundUplink" : true,
-      "statsOutboundDownlink" : true
-    }
+	"levels": {
+  	"0": {
+    	"statsUserDownlink": true,
+    	"statsUserUplink": true
+  	}
+	},
+	"system": {
+  	"statsInboundUplink": true,
+  	"statsInboundDownlink": true,
+  	"statsOutboundUplink" : true,
+  	"statsOutboundDownlink" : true
+	}
   }
 }
 END
@@ -359,7 +359,7 @@ After=network.target nss-lookup.target
 
 [Service]
 User=www-data
-CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE                                 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE                             	AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
 ExecStart=/usr/local/bin/xray run -config /etc/xray/config.json
 Restart=on-failure
@@ -388,18 +388,18 @@ EOF
 
 #nginx config
 cat >/etc/nginx/conf.d/xray.conf <<EOF
-    server {
-             listen 80;
-             listen [::]:80;
-             listen 443 ssl http2 reuseport;
-             listen [::]:443 http2 reuseport;	
-             server_name $domain;
-             ssl_certificate /etc/xray/xray.crt;
-             ssl_certificate_key /etc/xray/xray.key;
-             ssl_ciphers EECDH+CHACHA20:EECDH+CHACHA20-draft:EECDH+ECDSA+AES128:EECDH+aRSA+AES128:RSA+AES128:EECDH+ECDSA+AES256:EECDH+aRSA+AES256:RSA+AES256:EECDH+ECDSA+3DES:EECDH+aRSA+3DES:RSA+3DES:!MD5;
-             ssl_protocols TLSv1.1 TLSv1.2 TLSv1.3;
-             root /home/vps/public_html;
-        }
+	server {
+         	listen 80;
+         	listen [::]:80;
+         	listen 443 ssl http2 reuseport;
+         	listen [::]:443 http2 reuseport;    
+         	server_name $domain;
+         	ssl_certificate /etc/xray/xray.crt;
+         	ssl_certificate_key /etc/xray/xray.key;
+         	ssl_ciphers EECDH+CHACHA20:EECDH+CHACHA20-draft:EECDH+ECDSA+AES128:EECDH+aRSA+AES128:RSA+AES128:EECDH+ECDSA+AES256:EECDH+aRSA+AES256:RSA+AES256:EECDH+ECDSA+3DES:EECDH+aRSA+3DES:RSA+3DES:!MD5;
+         	ssl_protocols TLSv1.1 TLSv1.2 TLSv1.3;
+         	root /home/vps/public_html;
+    	}
 EOF
 sed -i '$ ilocation = /vlessws' /etc/nginx/conf.d/xray.conf
 sed -i '$ i{' /etc/nginx/conf.d/xray.conf
@@ -500,7 +500,7 @@ sed -i '$ i}' /etc/nginx/conf.d/xray.conf
 
 sleep 1
 echo -e "[ ${green}INFO$NC ] Installing bbr.."
-wget -q -O /usr/bin/bbr "https://raw.githubusercontent.com/myridwan/VVIP/ipuk/ssh/bbr.sh"
+wget -q -O /usr/bin/bbr "https://raw.githubusercontent.com/titis69/vpn4/main/ssh/bbr.sh"
 chmod +x /usr/bin/bbr
 bbr >/dev/null 2>&1
 rm /usr/bin/bbr >/dev/null 2>&1
@@ -515,8 +515,8 @@ systemctl enable runn
 systemctl restart runn
 
 sleep 1
-wget -q -O /usr/bin/auto-set "https://raw.githubusercontent.com/myridwan/VVIP/ipuk/xray/auto-set.sh" && chmod +x /usr/bin/auto-set 
-wget -q -O /usr/bin/crtxray "https://raw.githubusercontent.com/myridwan/VVIP/ipuk/xray/crt.sh" && chmod +x /usr/bin/crtxray 
+wget -q -O /usr/bin/auto-set "https://raw.githubusercontent.com/titis69/vpn4/main/xray/auto-set.sh" && chmod +x /usr/bin/auto-set
+wget -q -O /usr/bin/crtxray "https://raw.githubusercontent.com/titis69/vpn4/main/xray/crt.sh" && chmod +x /usr/bin/crtxray
 sleep 1
 yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
 yellow "xray/Vmess"
@@ -524,9 +524,12 @@ yellow "xray/Vless"
 
 
 
-mv /root/domain /etc/xray/ 
+mv /root/domain /etc/xray/
 if [ -f /root/scdomain ];then
 rm /root/scdomain > /dev/null 2>&1
 fi
 clear
 rm -f ins-xray.sh  
+
+
+
