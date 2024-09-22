da698daf80c84657e458c766eecf8d7ea8a1d04c
titis69 <117496052+titis69@users.noreply.github.com>
2024-09-22 16:50:47 +0700

    Update restore.sh

--- backup/restore.sh
+++ backup/restore.sh
@@ -4,54 +4,54 @@ biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 ###########- COLOR CODE -##############
 colornow=$(cat /etc/ssnvpn/theme/color.conf)
 NC="\e[0m"
-RED="\033[0;31m" 
+RED="\033[0;31m"
 COLOR1="$(cat /etc/ssnvpn/theme/$colornow | grep -w "TEXT" | cut -d: -f2|sed 's/ //g')"
-COLBG1="$(cat /etc/ssnvpn/theme/$colornow | grep -w "BG" | cut -d: -f2|sed 's/ //g')"                    
+COLBG1="$(cat /etc/ssnvpn/theme/$colornow | grep -w "BG" | cut -d: -f2|sed 's/ //g')"               	 
 ###########- END COLOR CODE -##########
 clear
 BURIQ () {
-    curl -sS  > /root/tmp
-    data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
-    for user in "${data[@]}"
-    do
-    exp=( `grep -E "^### $user" "/root/tmp" | awk '{print $3}'` )
-    d1=(`date -d "$exp" +%s`)
-    d2=(`date -d "$biji" +%s`)
-    exp2=$(( (d1 - d2) / 86400 ))
-    if [[ "$exp2" -le "0" ]]; then
-    echo $user > /etc/.$user.ini
-    else
-    rm -f /etc/.$user.ini > /dev/null 2>&1
-    fi
-    done
-    rm -f /root/tmp
+	curl -sS  > /root/tmp
+	data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
+	for user in "${data[@]}"
+	do
+	exp=( `grep -E "^### $user" "/root/tmp" | awk '{print $3}'` )
+	d1=(`date -d "$exp" +%s`)
+	d2=(`date -d "$biji" +%s`)
+	exp2=$(( (d1 - d2) / 86400 ))
+	if [[ "$exp2" -le "0" ]]; then
+	echo $user > /etc/.$user.ini
+	else
+	rm -f /etc/.$user.ini > /dev/null 2>&1
+	fi
+	done
+	rm -f /root/tmp
 }
 
 MYIP=$(curl -sS ipv4.icanhazip.com)
-Name=$(curl -sS https://raw.githubusercontent.com/myridwan/izinvps/ipuk/ip | grep $MYIP | awk '{print $2}')
+Name=$(curl -sS https://raw.githubusercontent.com/titis69/permission/main/ip | grep $MYIP | awk '{print $2}')
 echo $Name > /usr/local/etc/.$Name.ini
 CekOne=$(cat /usr/local/etc/.$Name.ini)
 
 Bloman () {
 if [ -f "/etc/.$Name.ini" ]; then
 CekTwo=$(cat /etc/.$Name.ini)
-    if [ "$CekOne" = "$CekTwo" ]; then
-        res="Expired"
-    fi
+	if [ "$CekOne" = "$CekTwo" ]; then
+    	res="Expired"
+	fi
 else
 res="Permission Accepted..."
 fi
 }
 
 PERMISSION () {
-    MYIP=$(curl -sS ipv4.icanhazip.com)
-    IZIN=$(curl -sS https://raw.githubusercontent.com/myridwan/izinvps/ipuk/ip | awk '{print $4}' | grep $MYIP)
-    if [ "$MYIP" = "$IZIN" ]; then
-    Bloman
-    else
-    res="Permission Denied!"
-    fi
-    BURIQ
+	MYIP=$(curl -sS ipv4.icanhazip.com)
+	IZIN=$(curl -sS https://raw.githubusercontent.com/titis69/permission/main/ip | awk '{print $4}' | grep $MYIP)
+	if [ "$MYIP" = "$IZIN" ]; then
+	Bloman
+	else
+	res="Permission Denied!"
+	fi
+	BURIQ
 }
 red='\e[1;31m'
 green='\e[0;32m'
@@ -72,19 +72,19 @@ fi
 
 cd
 MYIP=$(curl -sS ipv4.icanhazip.com)
-NameUser=$(curl -sS https://raw.githubusercontent.com/myridwan/izinvps/ipuk/ip | grep $MYIP | awk '{print $2}')
+NameUser=$(curl -sS https://raw.githubusercontent.com/titis69/permission/main/ip | grep $MYIP | awk '{print $2}')
 cekdata=$(curl -sS https://raw.githubusercontent.com/FranataVPN/backup/ipuk/$NameUser/$NameUser.zip | grep 404 | awk '{print $1}' | cut -d: -f1)
 
 echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
-echo -e "$COLOR1│${NC} ${COLBG1}             • RESTOR PANEL MENU •             ${NC} $COLOR1│$NC"
+echo -e "$COLOR1│${NC} ${COLBG1}         	• RESTOR PANEL MENU •         	${NC} $COLOR1│$NC"
 echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
 echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
 [[ "$cekdata" = "404" ]] && {
 red "│  [INFO] Data not found / you never backup"
-echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}" 
+echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
 echo -e "$COLOR1┌────────────────────── BY ───────────────────────┐${NC}"
-echo -e "$COLOR1│${NC}              • myridwan •                $COLOR1│$NC"
-echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}" 
+echo -e "$COLOR1│${NC}          	• myridwan •            	$COLOR1│$NC"
+echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
 echo ""
 read -n 1 -s -r -p "   Press any key to back on menu"
 menu
@@ -136,10 +136,13 @@ rm -rf /root/backup &> /dev/null
 echo -e "$COLOR1│${NC}  [ ${green}INFO${NC} ] • Done... Successfully - Script By FranataSTORE."
 sleep 1
 rm -f /root/backup/backup.zip &> /dev/null
-echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}" 
+echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
 echo -e "$COLOR1┌────────────────────── BY ───────────────────────┐${NC}"
-echo -e "$COLOR1│${NC}              • myridwan •                $COLOR1│$NC"
-echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}" 
+echo -e "$COLOR1│${NC}          	• myridwan •            	$COLOR1│$NC"
+echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
 echo ""
 read -n 1 -s -r -p "   Press any key to back on menu"
 menu-backup
+
+
+
