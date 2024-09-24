823a19224f5c7a9facf3b061994e8e5e72d08a60
titis69 <117496052+titis69@users.noreply.github.com>
2024-09-24 11:33:42 +0700

    Update menu-vmess.sh

--- update/menu-vmess.sh
+++ update/menu-vmess.sh
@@ -311,6 +311,7 @@ echo -e "\033[1;93m〔⎆〕 ${grenbo}1.${NC} \033[0;36mCreating a Vmess Account
 echo -e "\033[1;93m〔⎆〕 ${grenbo}2.${NC} \033[0;36mRenew a Vmess Account${NC}"
 echo -e "\033[1;93m〔⎆〕 ${grenbo}3.${NC} \033[0;36mDelete Vmess Account${NC}"
 echo -e "\033[1;93m〔⎆〕 ${grenbo}4.${NC} \033[0;36mCheck Vmess Account${NC}"
+echo -e "\033[1;93m〔⎆〕 ${grenbo}5.${NC} \033[0;36mKembali Menu Account${NC}"
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" 
 echo -e ""
 read -p " Select menu :  "  opt
